9a49f8d199bcf139a0e7740c7e149e1c1213c323
github username <github email address>
2020-01-29 16:10:21 -0500

    finished with instance methods

--- lib/owner.rb
+++ lib/owner.rb
@@ -1,3 +1,44 @@
 class Owner
-  # code goes here
+  attr_reader :name, :species
+  @@owners = []
+  
+  def initialize(name)
+    @name = name
+    @species = "human"
+    @@owners << self
+  end
+  
+  def say_species
+    "I am a #{@species}."
+  end
+
+  #owner can buy cat
+  def buy_cat(name)
+  end
+
+  #owner can buy dog
+  def buy_dog(name)
+  end
+
+  def get_cat
+  end
+
+  def self.all
+    @@owners
+  end
+
+  def self.count
+    @@owners.length
+  end
+
+  def self.reset_all
+    @@owners = []
+  end
+
+  def cats
+    
+  end
+
 end
+
+#owner.name = name
